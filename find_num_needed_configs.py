790c66435e88da921f9f97e5c76eaaca75e7fe42
danieli4444 <d4444i@gmail.com>
2022-11-21 01:48:56 -0800

    added more plots

--- find_num_needed_configs.py
+++ find_num_needed_configs.py
@@ -17,7 +17,8 @@ def create_chart(ci_mat_dir,compared_energy,chart_name):
     x = os.listdir(ci_mat_dir)
     for ci_mat_file in os.listdir(ci_mat_dir):
         configs_num = ci_mat_file.replace('.out','')
-        configs_num = [int(s) for s in configs_num.split('_') if s.isdigit()][-1]
+        x = configs_num.split('_')
+        configs_num = [int(s) for s in configs_num.split('_') if s.isdigit()][0]
 
         ci_mat_path = ci_mat_dir + '/' + ci_mat_file
         ci_mat = np.loadtxt(ci_mat_path)
@@ -30,19 +31,18 @@ def create_chart(ci_mat_dir,compared_energy,chart_name):
     
     
 
-    plt.figure()
+    fig = plt.figure(figsize=(8,5.2))
     # plt.errorbar(counts,exact,yerr=self.chemical_accuracy,label='exact',linestyle="dashed",color='green',ecolor = 'black',capsize=3)
     # plt.plot(num_configs,exact,label='exact',linestyle="",marker="o")
     # plt.plot(num_configs,fci,label='fci',linestyle="dotted")
     # plt.xticks(counts)
-    plt.xlabel("number of configurations")
+    plt.xlabel("Number of configurations")
     plt.ylabel("Energy [Ha]")
     
     plt.xticks(num_configs_list,num_configs_list)
-    plt.plot(num_configs_list,ci_energy_list,'o',label='selected CI')
-    plt.errorbar(num_configs_list,len(num_configs_list)*[compared_energy], yerr = CHEMICAL_ACCURACY,ecolor = 'green',label='full diagonlization',capsize=3)
+    plt.plot(num_configs_list,ci_energy_list,'o',label='Selected CI')
+    plt.errorbar(num_configs_list,len(num_configs_list)*[compared_energy], yerr = CHEMICAL_ACCURACY,ecolor = 'green',label='Full CI',capsize=3)
     plt.legend()
-
     # text = "exact ci E=" + str(round(ci_ground_energy,8)) + ' vqe best=' + str(round(self.optimal_energy ,8))
     
     filename = ci_mat_dir + '/' + chart_name + '_energy_vs_configs.png'
@@ -63,6 +63,6 @@ if __name__ == "__main__":
 
 
     ci_mat_dir = 'BeH2_1.3/CI_matrices'
-    fci_energy = -15.595047080804692- 3.4600048389961535
+    fci_energy = -15.595047080804692 - 3.4600048389961535
     chart_name = 'BeH2_1.3'
     create_chart(ci_mat_dir,fci_energy,chart_name)
---
+++ test.py
@@ -0,0 +1,2 @@
+import qiskit.aqua
+print(qiskit.__version__)
